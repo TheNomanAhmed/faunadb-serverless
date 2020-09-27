7c78d01db47cb3eb5f686d25758428ab80ecc00f
thenomanahmed <nomanahmed478@gmail.com>
2020-09-27 19:47:09 +0300

    wip: completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import LinkList from "./components/LinkList";
+import LinkForm from "./components/LinkForm";
+//Grab all of the links
+//display all of the links
+//add delete and archive functionality
 function App() {
+  const [links, setLinks] = useState([]);
+  const loadLinks = async () => {
+    try {
+      const res = await fetch("/.netlify/functions/getLinks");
+      const links = await res.json();
+      setLinks(links);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    loadLinks();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container py-5">
+      <h1 className="text-center mb-5">List O' Link</h1>
+      <LinkForm refreshLinks={loadLinks} />
+      <LinkList links={links} refreshLinks={loadLinks} />
     </div>
   );
 }
